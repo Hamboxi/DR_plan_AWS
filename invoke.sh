d0141994f72d3dccd24433a4a30ef3529eaa84a1
Hamboxi <flavio_only@hotmail.com>
2023-09-19 06:25:55 +0000

    Inclusao de arquivos de invocacao

---
+++ invoke.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Defina os parâmetros da função Lambda
+function_name="VPC-SG-CloudFormation"
+input_payload={
+    "key1": "value1",
+    "key2": "value2"} 
+# Substitua pelos dados que você deseja enviar para a função
+
+# Comando para invocar a função Lambda
+aws lambda invoke --function-name "$function_name" --payload "$input_payload" output.json
+
+# Verifique a saída (output.json)
+cat output.json
